db43a4f0f0cf89f901dee78ceb4ba3d0d2429d97
IT projects team <itprotjects@gmail.com>
2026-04-23 05:59:20 +0200

    Update Boostore.js-mohamed-fix theme when refresh page

--- JS/Boostore.js
+++ JS/Boostore.js
@@ -2,14 +2,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeBtn = document.querySelector('.theme-change');
     const themeLink = document.querySelector('#theme-link');
 
+    // check if theme was already saved
+    const savedTheme = localStorage.getItem('theme');
+
+
+    if (savedTheme) {
+        themeLink.setAttribute('href', savedTheme);
+    }
+
     themeBtn.addEventListener('click', () => {
-        // Check what the current file is
-        if (themeLink.getAttribute('href') === 'css/Boostore.css') {
-            // Switch to dark mode
-            themeLink.setAttribute('href', 'css/dark-mode.css');
-        } else {
-            // Switch back to light mode
-            themeLink.setAttribute('href', 'css/Boostore.css');
+        let currentTheme = themeLink.getAttribute('href');
+        let newTheme;
+
+        if (currentTheme === 'css/Boostore.css') { //if theme is light mode
+            newTheme = 'css/dark-mode.css';
+        } else { //if theme is not light mode 
+            newTheme = 'css/Boostore.css';
         }
+
+        // implement the new theme and save the choice to localStorage
+        themeLink.setAttribute('href', newTheme);
+        localStorage.setItem('theme', newTheme);
     });
 });
