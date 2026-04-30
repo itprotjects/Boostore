65be9eea37ecc73093c0cefaa2a130724cfaad5a
IT projects team <itprotjects@gmail.com>
2026-04-30 23:29:54 +0300

    mohamed- add data/theme changer/books render JS

---
+++ JS/Boostore.js
@@ -0,0 +1,23 @@
+
+const container = document.getElementById("featuredBooks");
+
+if (container) {
+  const featured = books.slice(0, 7);
+
+  featured.forEach(book => {
+    const card = document.createElement("div");
+    card.classList.add("book-card");
+
+    card.innerHTML = `
+      <img src="${book.cover}" alt="${book.title}">
+      <h3>${book.title}</h3>
+      <p>${book.author}</p>
+      <a href="details.html?id=${book.id}">View</a>
+    `;
+
+    container.appendChild(card);
+  });
+}
+
+
+
